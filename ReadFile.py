88f43123e48307c0043fa30aa4164a9f1ec3a7ea
Joseph Hickey <JosephRHickey@Outlook.com>
2020-01-29 18:27:30 -0700

    Added Homework2 and code.
    ReadFile and ParticleProperties python scripts written in jupytr notebook.

---
+++ ReadFile.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Joseph Hickey  
+# ASTR400B Homework 2  
+# 1/29/2020  
+# Write code that can read in the data from the MW_000.txt file  
+
+# In[5]:
+
+
+import numpy as np
+import astropy.units as u
+
+def Read(filename):
+    
+#Open file in same directory
+    file = open(filename,'r')
+#Readout first line and split it into component word and value. Assign units to value
+    line1 = file.readline()
+    label, value = line1.split()
+    time = float(value)*u.Myr
+#Readout second line and split it and assign units.
+    line2 = file.readline()
+    label, value = line2.split()
+    N = float(value)
+#Close file
+    file.close()
+#Put all the other data into a sorted ordered array
+    data = np.genfromtxt(filename,dtype=None,names=True,skip_header=3)
+    
+    return time, N, data
+
